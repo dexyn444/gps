ad745c5cbb9c44f525858ac1a592c45877f09aba
dexyn444 <105306981+dexyn444@users.noreply.github.com>
2025-08-16 18:11:36 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+  "name": "Rastro Seguro",
+  "short_name": "Rastro",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#0078d7",
+  "theme_color": "#0078d7",
+  "icons": [
+    {"src":"icon-72.png","sizes":"72x72","type":"image/png"},
+    {"src":"icon-96.png","sizes":"96x96","type":"image/png"},
+    {"src":"icon-128.png","sizes":"128x128","type":"image/png"},
+    {"src":"icon-192.png","sizes":"192x192","type":"image/png"},
+    {"src":"icon-512.png","sizes":"512x512","type":"image/png"}
+  ]
+}
